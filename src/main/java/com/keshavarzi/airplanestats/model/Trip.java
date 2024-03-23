edae793fbc782d65cb9ca1f4d0917d06076c3ba4
Parham Keshavarzi <none@>
2024-03-23 16:51:27 -0500

    Update all model classes

---
+++ src/main/java/com/keshavarzi/airplanestats/model/Trip.java
@@ -0,0 +1,44 @@
+package com.keshavarzi.airplanestats.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Table;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Getter @Setter @ToString
+@Entity @Table(name = "trip")
+public class Trip {
+
+    @Id
+    @Column(name="trip_id", unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_email", referencedColumnName = "email", nullable = false, table = "trip")
+    private User user;
+
+    @OneToMany(mappedBy = "trip", targetEntity = Route.class)
+    private List<Route> routes;
+
+    @Column(name = "trip_name", table = "trip")
+    private String tripName;
+
+    @Column(name = "trip_start", table = "trip")
+    private ZonedDateTime tripStart;
+
+    @Column(name = "trip_end", table = "trip")
+    private ZonedDateTime tripEnd;
+}
--- src/main/java/com/keshavarzi/airplanestats/model/User.java
+++ src/main/java/com/keshavarzi/airplanestats/model/User.java
@@ -3,19 +3,28 @@ package com.keshavarzi.airplanestats.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Entity
-@Table(name="users")
+import java.util.List;
+
+@Getter @Setter @ToString
+@Entity @Table(name="users")
 public class User {
 
-    @Data
-    @Column(unique = true)
-    private String emailAddress;
+    @Id
+    @Column(name = "email", unique = true, nullable = false, table = "users")
+    private String email;
 
-    @Data
     @Column
     private String password;
 
+    // Exclusion is because associations impact performance
+    @OneToMany(mappedBy = "user", targetEntity = Trip.class)
+    @ToString.Exclude
+    private List<Trip> savedRoutes;
 }
