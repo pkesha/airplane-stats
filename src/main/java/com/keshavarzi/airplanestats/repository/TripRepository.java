5251dc8278b33bda5d7326589bd956093a53f959
Parham Keshavarzi <none@>
2024-03-26 15:08:26 -0500

    Update all model classes

---
+++ src/main/java/com/keshavarzi/airplanestats/repository/TripRepository.java
@@ -0,0 +1,13 @@
+package com.keshavarzi.airplanestats.repository;
+
+import com.keshavarzi.airplanestats.model.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TripRepository extends JpaRepository<Trip, Long> {
+    List<Trip> findAllByUser(String email);
+    Trip findTripById(Long Id);
+    List<Trip> findTripByRoutes(Long routeId);
+
+}
--- src/main/java/com/keshavarzi/airplanestats/repository/UserRepository.java
+++ src/main/java/com/keshavarzi/airplanestats/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.keshavarzi.airplanestats.repository;
 
-public class UserRepository {
+import com.keshavarzi.airplanestats.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    User findUserByEmail(String email);
+    boolean deleteUserByEmail(String email);
+    boolean updateUserByEmail(String email);
+
 }
